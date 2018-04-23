bd566eafaf758982856d9f9ddeacfd48f7ac9d0e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to custom-draw the Current Time Marker
+
+
+<p>This example illustrates how to draw the Current Time Marker in the <a href="http://documentation.devexpress.com/#WindowsForms/CustomDocument1737"><u>Time Ruler</u></a>. To accomplish this task, handle the <a href="http://documentation.devexpress.com/#WindowsForms/DevExpressXtraSchedulerSchedulerControl_CustomDrawDayViewTimeRulertopic"><u>SchedulerControl.CustomDrawDayViewTimeRuler Event</u></a> (calculate the Current Time Marker position and draw it manually in this event handler). Also, set the <a href="https://documentation.devexpress.com/#CoreLibraries/DevExpressXtraSchedulerTimeRuler_TimeMarkerVisibilitytopic">TimeRuler.TimeMarkerVisibility </a> to <strong>Never</strong> to suppress drawing of a standard Current Time Marker.</p>
+<p><strong>See also:<br> </strong><a href="https://www.devexpress.com/Support/Center/p/E2469">How to draw a horizontal line indicating current time</a></p>
+
+<br/>
+
+
---
+++ VB/Form1.vb
@@ -0,0 +1,43 @@
+﻿Imports System
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports DevExpress.XtraScheduler
+Imports DevExpress.XtraScheduler.Drawing
+
+Namespace SchedulerCustomDrawCurrentTimeMarker
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+            schedulerControl1.DayView.TimeRulers(0).TimeMarkerVisibility = TimeMarkerVisibility.Never
+
+        End Sub
+
+        Private Sub schedulerControl1_CustomDrawDayViewTimeRuler(ByVal sender As Object, ByVal e As DevExpress.XtraScheduler.CustomDrawObjectEventArgs) Handles schedulerControl1.CustomDrawDayViewTimeRuler
+            Dim scheduler As SchedulerControl = DirectCast(sender, SchedulerControl)
+            Dim now As TimeSpan = Date.Now.TimeOfDay
+            Dim viewInfo As TimeRulerViewInfo = CType(e.ObjectInfo, TimeRulerViewInfo)
+            Dim rect As Rectangle = viewInfo.ContentBounds
+            Dim nowRect As Rectangle = rect
+            Dim dayViewInfo As DayViewInfo = TryCast(scheduler.DayView.ViewInfo, DayViewInfo)
+
+            If dayViewInfo.VisibleRowsCount = 0 Then
+                Return
+            End If
+
+            Dim offsetRatio As Single = (now - scheduler.DayView.TopRowTime).Ticks / CSng(TimeSpan.FromTicks(scheduler.DayView.TimeScale.Ticks * dayViewInfo.VisibleRowsCount).Ticks)
+
+            If offsetRatio < 0 Then
+                Return
+            End If
+
+            nowRect.Height = 4
+            nowRect.Y += CInt((Math.Round(rect.Height * offsetRatio - CSng(nowRect.Height) / 2)))
+
+            e.DrawDefault()
+            e.Cache.FillRectangle(Brushes.Red, nowRect)
+            e.Handled = True
+        End Sub
+    End Class
+End Namespace
